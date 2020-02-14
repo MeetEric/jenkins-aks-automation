d5725c58820ac44a3ecf219c8ccf0b777a991be8
Eric Maino <eric.maino@microsoft.com>
2020-02-14 09:29:01 +0100

    wip

--- aks-terraform/tf/test.tf
+++ aks-terraform/tf/test.tf
@@ -9,3 +9,9 @@ module "init" {
     source = "./modules/azure"
     config = module.config
 }
+
+module "network" {
+    source = "./modules/azure/network"
+    config = module.config
+    name = "test-vnet"
+}
